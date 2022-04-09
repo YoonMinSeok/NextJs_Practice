6f059b3f25b11f3bf6df4cd7640f3a1152553242
윤민석 <ykmvm147@naver.com>
2022-04-09 13:51:42 +0900

    request api response data

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+
+//api/new-meetup
+
+async function handler(res, req) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://ykmvm147:alstjr126@cluster0.7huik.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+    res.status(201).json({ message: "Meetup inserted" });
+  }
+}
+
+export default handler;
